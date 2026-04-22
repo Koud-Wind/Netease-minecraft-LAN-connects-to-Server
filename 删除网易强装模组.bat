1faa29964e394ad8b74911ea52ce09597c1c9b4d
Koud-Wind <cold1950963108@outlook.com>
2026-04-22 21:32:24 +0800

    2.4.1 更新

--- 删除网易强装模组.bat
+++ 删除网易强装模组.bat
@@ -3,8 +3,17 @@ setlocal enabledelayedexpansion
 
 REM 下面是网易模组目录
 set "targetDir=C:\MCLDownload\Game\.minecraft\mods"
+set "targetDir2=C:\netease_minecraft_neoforge\mods"
 
 for %%F in ("%targetDir%\*") do (
+    set "fileName=%%~nF"
+    REM 名称末尾为@0就会删除
+    if "!fileName:~-2!"=="@0" (
+        del "%%F"
+    )
+)
+
+for %%F in ("%targetDir2%\*") do (
     set "fileName=%%~nF"
     REM 名称末尾为@0就会删除
     if "!fileName:~-2!"=="@0" (
